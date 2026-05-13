e5eb51cea340b9952c7ff202352f0eb1bc33f605
IMora04 <ignmorper1@alum.us.es>
2026-05-13 11:48:44 +0200

    Test volume change

--- sound_program.py
+++ sound_program.py
@@ -5,7 +5,7 @@ import pyaudio
 
 # --- CONFIGURACIÓN ---
 FRECUENCIA = 440       # Frecuencia en Hz (Nota LA)
-PORCENTAJE_VOLUMEN = 30  # Variable de volumen: define el porcentaje (0 a 100)
+PORCENTAJE_VOLUMEN = 80  # Variable de volumen: define el porcentaje (0 a 100)
 RATE = 44100            # Tasa de muestreo (Calidad de CD)
 CHANNELS = 2            # CONFIGURACIÓN MANDATORIA: 2 Canales para Estéreo
 INTERVALO = 1.0        # Tiempo en segundos para alternar de lado
